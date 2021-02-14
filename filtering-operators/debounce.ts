430b714efee51095e5a4feea293499be6f06a592
jatinpassi <jatinpassi111@gmail.com>
2021-02-14 17:52:38 +0530

    2 files modified

--- filtering-operators/debounce.ts
+++ filtering-operators/debounce.ts
@@ -1,6 +1,9 @@
 import { interval, timer, fromEvent } from "rxjs";
 import { debounce, take } from "rxjs/operators";
 
+//throttle vs debouncing
+//The main difference between throttling and debouncing is that throttling executes the function at a regular interval, while debouncing executes the function only after some cooling period.
+
 export const debounce$ = fromEvent(
   document.getElementById("myInput"),
   "keyup"
--- filtering-operators/throttle.ts
+++ filtering-operators/throttle.ts
@@ -1,6 +1,9 @@
 import { throttle } from "rxjs/operators";
 import { fromEvent, interval } from "rxjs";
 
+//throttle vs debouncing
+//The main difference between throttling and debouncing is that throttling executes the function at a regular interval, while debouncing executes the function only after some cooling period.
+
 export const throttle$ = fromEvent(
   document.getElementById("myInput"),
   "keyup"

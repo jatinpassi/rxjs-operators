6bdcc3686dce6a120e3ef4c2c94604dd6442ca5a
jatinpassi <jatinpassi111@gmail.com>
2021-02-15 10:16:31 +0530

    2 files modified

--- filtering-operators/find.ts
+++ filtering-operators/find.ts
@@ -1,5 +1,13 @@
-import { find } from "rxjs/operators";
-import { interval } from "rxjs";
+import { find, mapTo } from "rxjs/operators";
+import { interval, fromEvent } from "rxjs";
 
 // If you always want the first item emitted, regardless of condition.
 export const find$ = interval(1000).pipe(find(ele => ele % 5 === 0));
+
+export const find1$ = fromEvent(
+  document.getElementById("myInput"),
+  "keyup"
+).pipe(
+  find((e: any) => e.target.value === "jatin"),
+  mapTo("Found")
+);
--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
-import { find$ } from "./filtering-operators/find";
-find$.subscribe(e => console.log("find:", e));
+import { find$, find1$ } from "./filtering-operators/find";
+// find$.subscribe(e => console.log("find:", e));
+find1$.subscribe(e => console.log("find1:", e));
